95a38345e6e187a73f830dbf1fa19b4a28168903
Patrick <105521908+blackrainz@users.noreply.github.com>
2022-06-19 17:39:29 -0700

    Add files via upload
    Completed party 4 of Challenge 1

--- GitHub_Upload/loan_analyzer.py
+++ GitHub_Upload/loan_analyzer.py
@@ -78,7 +78,7 @@ print("The remaining months on the loan is:", + remaining_months, "months")
 # YOUR CODE HERE!
 discount_rate = .2
 present_value = future_value / (1 + discount_rate/12) ** remaining_months
-
+print("The present value of the loan is:", + present_value)
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
 #    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
@@ -115,12 +115,24 @@ new_loan = {
 #    The function should return the `present_value` for the loan.
 # YOUR CODE HERE!
 
+def calculate_present_value(future_value, remaining_months, annual_discount_rate):
+    annual_discount_rate = (annual_discount_rate)
+    future_value = (future_value)
+    print("The future value of the loan is:", + future_value, "$")
+    remaining_months = (remaining_months)
+    print("The remaining months on the loan is:", + remaining_months, "months")
+    present_value = future_value / (1 + annual_discount_rate/12) ** remaining_months
+    return present_value
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
+present_value = calculate_present_value(
+    new_loan["future_value"],
+    new_loan["remaining_months"],
+    annual_discount_rate = 0.2
+)
 print(f"The present value of the loan is: {present_value}")
-
 
 """Part 4: Conditionally filter lists of loans.
 
@@ -162,13 +174,15 @@ loans = [
 
 # @TODO: Create an empty list called `inexpensive_loans`
 # YOUR CODE HERE!
-
+inexpensive_loans = []
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
 # YOUR CODE HERE!
-
+for loan_price in loans:
+    if loan_price["loan_price"] <= 500:
+        inexpensive_loans.append(loan_price)
 # @TODO: Print the `inexpensive_loans` list
 # YOUR CODE HERE!
-
+print(inexpensive_loans)
 
 """Part 5: Save the results.
 
